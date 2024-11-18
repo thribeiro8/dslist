d96442274e6904b5a4f682104425105729304089
Thomas Ribeiro <thomas18rs@gmail.com>
2024-11-18 17:52:49 -0300

    feat: add services

---
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.devsuperior.dslist.dtos.GameDTO;
+import com.devsuperior.dslist.dtos.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
+import com.devsuperior.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(@PathVariable Long listID) {
+        Game game = gameRepository.findById(listID).get();
+        return new GameDTO(game);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findAll() {
+        List<Game> games = gameRepository.findAll();
+        return games.stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByGameList(Long listId) {
+        List<GameMinProjection> gamesProjection = gameRepository.searchByList(listId);
+        return gamesProjection.stream().map(GameMinDTO::new).toList();
+    }
+}
