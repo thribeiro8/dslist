051e72f4af2e21261240dc2ef795f790a00a2ee8
Thomas Ribeiro <thomas18rs@gmail.com>
2024-11-18 17:51:12 -0300

    feat: add controllers

---
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslist.dtos.GameDTO;
+import com.devsuperior.dslist.dtos.GameMinDTO;
+import com.devsuperior.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
+    }
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        return gameService.findAll();
+    }
+}
---
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -0,0 +1,48 @@
+package com.devsuperior.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslist.dtos.GameListDTO;
+import com.devsuperior.dslist.dtos.GameMinDTO;
+import com.devsuperior.dslist.dtos.ReplacementDTO;
+import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+    @Autowired
+    private GameListService gameListService;
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameListDTO findById(@PathVariable Long id) {
+        return gameListService.findById(id);
+    }
+
+    @GetMapping
+    public List<GameListDTO> findAll() {
+        return gameListService.findAll();
+    }
+
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findGames(@PathVariable Long listId) {
+        return gameService.findByGameList(listId);
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+    }
+}
